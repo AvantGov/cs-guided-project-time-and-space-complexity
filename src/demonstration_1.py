8cbf07660814e78efb6863b92426258ff4fd0d7f
avantgov <alexcouts18@gmail.com>
2020-10-05 20:33:30 -0500

    completed solutions

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -18,7 +18,70 @@ solving out-of-place, try an in-place solution with a space complexity of O(1).
 For that solution, you will need to return the length of the modified `nums`.
 The length will tell the user where the end of the array is after removing all
 of the duplicates.*
-"""
-def remove_duplicates(nums):
-    # Your code here
 
+
+
+UPER 
+
+UNDERSTAND 
+- our goal is to remove the duplicates
+- the array is already sorted
+- numbers don't have to be consecuctive 
+- assumed to all be integers 
+
+PLAN 
+- turn the list into a set --> automatically remove duplicates (what sets do)
+- turn into a dictionary --> dictionaries can't have more than one explicit key 
+- loop in a loop to check each variable against the other variables 
+
+EXECUTE
+"""
+
+# turn the list into a set --> automatically remove duplicates (what sets do)
+# RUNTIME: O(n)
+# SPACE: O(n)
+def remove_duplicates(nums):
+    nums_set = set(nums)
+    # or
+    for nums in nums:
+        nums_set.add(num)
+
+    return list(nums_set)
+
+
+# loop in a loop to check each variable against the other variables 
+# RUNTIME: O(1) + O(n ^ O(1) + O(n)) --> O(n ^ 2)
+# SPACE: O(1) --> there is no space used 
+def remove_duplicates(nums):
+    # in place means we don't use extra space -- 
+    # ie we have to swap / move nums around in the given 
+    i = 0                           # O(1)
+    while i < len(nums) - 1:        # runs how many times? O(n)
+        if nums[i] == nums[i+1]:    # O(1)
+            nums.pop(i+1)           # O(n)
+        else:
+            i = i + 1
+    return nums 
+
+
+#RUNTIME: O(n) --> since the list is only being iterated through once 
+# (even with two indecies)
+def remove_duplicates_in_place_linear(nums):
+    # keep one idx pointing to the end of the part of the array with duplicates 
+    non_duplicate_index = 0  
+
+    # and another idx pointing to the current element in the array
+    # iterate through the list 
+    for current_index in range(len(nums)):
+        # if the current element is already in the array
+        if nums[current_index] == nums[current_index]:
+            # skip it
+            continue
+        else:
+            # otherwise, set nums[num_duplicate_index] to the current element 
+            nums[non_duplicate_index] = current[current_index]
+            # and incremement non_duplicate_index
+            non_duplicate_index += 1 
+    
+    return nums, non_duplicate_index
+    
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -19,6 +19,47 @@ time complexities of `O(n log n)` or `O(n)`. Possible space complexities are
 `O(1)` or `O(n)`. Try to come up with solutions with different time and space
 complexities and think about the tradeoffs between the solutions.*
 """
-def contains_duplicate(nums):
-    # Your code here
 
+
+"""
+UNDERSTAND 
+- return true if there are any repeated values 
+- return false if all are unique 
+
+PLAN
+
+#1
+- make a new set 
+- if the set length wasn't equal to the array length, that means there were duplicates
+    - TIME: O(n)
+    - SPACE: O(n)
+
+#2
+- sort the given set of numbers 
+- iterate over the sorted list of items 
+    - for each item, compare it to the next item 
+    - if they are the same, then return True 
+- if True is never returned, return False 
+
+"""
+
+# PLAN # 2
+# TIME: O(n log n + n) --> O(n log n)
+# SPACE: 
+def contains_duplicates(nums):
+    # create count iterator
+    i = 0                   # O(1)
+   
+    #sort the nums given, in place  
+    nums.sort()             # O(n log n)
+
+    # for each num,
+    while i < len(nums) - 1:    #O(n)
+        # compare the num next to current num
+        if nums[1] == nums[i+1]:        # O(1)    
+            return True
+            i += 1
+        else: 
+            i += 1
+            continue
+    return False
